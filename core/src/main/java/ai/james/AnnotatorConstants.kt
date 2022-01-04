4c72d31436cc00aaca31edd36e60dff02985b37d
James Zow <jameszow@163.com>
2022-01-04 11:28:47 +0800

    Add PropertiesFactory Annotator

---
+++ core/src/main/java/ai/james/AnnotatorConstants.kt
@@ -0,0 +1,32 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 James Zow
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software
+ */
+package ai.james
+
+interface AnnotatorConstants {
+
+        companion object {
+            const val pos: String = "tokenize,ssplit";
+
+            const val lemma: String = "tokenize,ssplit,pos";
+
+            const val ner: String = "tokenize,ssplit,pos,lemma";
+
+            const val regexner: String = "tokenize,ssplit,pos";
+
+            const val sentiment: String = "tokenize,ssplit,pos,parse";
+
+            const val parse: String = "tokenize,ssplit,parse";
+
+            const val depparse: String = "tokenize,ssplit,pos";
+        }
+
+}
---
+++ core/src/main/java/ai/james/PropertiesFactory.kt
@@ -0,0 +1,28 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 James Zow
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software
+ */
+package ai.james
+
+import java.util.*
+
+class PropertiesFactory {
+
+    var key     : String? = null;
+    var value   : String? = null;
+
+    companion object{
+        fun setAnnotator(key: String, value: String): Properties {
+            val properties = Properties();
+            properties.setProperty(key, value);
+            return properties;
+        }
+    }
+}
