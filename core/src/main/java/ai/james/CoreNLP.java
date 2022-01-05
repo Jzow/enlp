2336587fa31862bc8023cae16561aebff765049b
James Zow <jameszow@163.com>
2022-01-05 11:01:12 +0800

    add CoreNLP

---
+++ core/src/main/java/ai/james/CoreNLP.java
@@ -0,0 +1,16 @@
+package ai.james;
+
+import edu.stanford.nlp.pipeline.AnnotationPipeline;
+import edu.stanford.nlp.pipeline.AnnotatorPool;
+import edu.stanford.nlp.util.PropertiesUtils;
+
+import java.util.Properties;
+
+/**
+ * @Author: James Zow
+ * @Date: 2022/01/04/14:03
+ * @Description:
+ */
+public class CoreNLP extends AnnotationPipeline {
+
+}
