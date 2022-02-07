5211b68c3232c75650899e6f9aae88397c3e0be4
jameszow <jameszow@163.com>
2022-02-08 02:16:24 +0800

    Add ApacheNLP test

---
+++ word/src/test/java/ai/james/ApacheNLPTest.java
@@ -0,0 +1,25 @@
+package ai.james;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * /**
+ * 描述:
+ *
+ * @author James Zow
+ * @create 2022/1/16
+ */
+public class ApacheNLPTest {
+
+    public static void main(String[] args) {
+        try {
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
