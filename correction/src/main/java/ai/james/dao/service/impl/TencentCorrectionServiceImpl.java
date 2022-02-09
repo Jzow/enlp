0db9690b754027e2864e5407e55ef4bdd75e5849
James Zow <jameszow@163.com>
2022-02-09 13:49:39 +0800

    [Add] add MIT License for correction module

--- correction/src/main/java/ai/james/dao/service/impl/TencentCorrectionServiceImpl.java
+++ correction/src/main/java/ai/james/dao/service/impl/TencentCorrectionServiceImpl.java
@@ -1,5 +1,15 @@
 package ai.james.dao.service.impl;
-
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 James Zow
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software
+ */
 import ai.james.dao.mapper.TencentCorrectionMapper;
 import ai.james.dao.service.TencentCorrectionService;
 import ai.james.model.dto.QueryPageTencentReportDTO;
