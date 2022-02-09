164016b2c74f94dab643212604ad8627e27fab6f
James Zow <jameszow@163.com>
2022-02-09 13:03:28 +0800

    [Add] add how todo tobe symbol

--- nlpAlgorithm/src/main/java/ai/james/utils/constants/RegexConstants.kt
+++ nlpAlgorithm/src/main/java/ai/james/utils/constants/RegexConstants.kt
@@ -4,6 +4,16 @@ interface RegexConstants {
 
     companion object {
 
-        const val empty: String  =   " ";
+        const val empty: String         =   " ";
+
+        const val WhSymbol: String      =   "Wh";
+
+        const val howSymbol: String     =   "how";
+
+        const val todoSymbol: String    =   "to do";
+
+        const val tobeSymbol: String    =   "to be";
+        // while 没有   AUX:系动词
+        val whSymbol                    =   listOf("what","where","when","why","who","whom","which","whether")
     }
 }
